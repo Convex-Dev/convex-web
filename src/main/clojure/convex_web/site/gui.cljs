a9297f57f281ad719918c7566a54e7eb78509294
Dee Pheap <pheapdee35@gmail.com>
2020-08-26 17:51:07 +0700

    Delete unused code

--- src/main/clojure/convex_web/site/gui.cljs
+++ src/main/clojure/convex_web/site/gui.cljs
@@ -43,17 +43,6 @@
           attrs)
    [:path {:d "M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"}] [:path {:d "M21 12a9 9 0 11-18 0 9 9 0 0118 0z"}]])
 
-(defn TagIcon [& [attrs]]
-  [:svg
-   (merge {:fill "none"
-           :stroke-linecap "round"
-           :stroke-linejoin "round"
-           :stroke-width "2"
-           :viewBox "0 0 24 24"
-           :stroke "currentColor"}
-          attrs)
-   [:path {:d "M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"}]])
-
 (defn SortAscendingIcon [& [attrs]]
   [:svg
    (merge {:fill "none"
@@ -131,18 +120,6 @@
           attrs)
    [:path {:d "M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"}]])
 
-(defn IconEye [& [attrs]]
-  [:svg
-   (merge {:fill "none"
-           :stroke-linecap "round"
-           :stroke-linejoin "round"
-           :stroke-width "2"
-           :viewBox "0 0 24 24"
-           :stroke "currentColor"}
-          attrs)
-   [:path {:d "M15 12a3 3 0 11-6 0 3 3 0 016 0z"}]
-   [:path {:d "M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"}]])
-
 (defn IconUser [& [attrs]]
   [:svg
    (merge {:fill "none"
@@ -153,15 +130,6 @@
            :viewBox "0 0 24 24"}
           attrs)
    [:path {:d "M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"}]])
-
-(defn IconButtonCode [{:keys [color hover-background on-click] :or {on-click identity}}]
-  [:a.rounded-full.h-8.w-8.flex.items-center.justify-center.text-xl.tooltip
-   {:class (or hover-background "hover:bg-gray-300")
-    :aria-label "Copy to editor"
-    :data-balloon-pos "up"
-    :on-click on-click}
-   [:i.zmdi.zmdi-code
-    {:class (or color "text-gray-700")}]])
 
 (defn Highlight [source]
   [:div.shadow.overflow-auto
