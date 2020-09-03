3b26a466e9affe9eaef694fa5616dd33204c728b
Dee Pheap <pheapdee35@gmail.com>
2020-09-03 15:31:45 +0700

    Welcome nav WIP

--- src/main/clojure/convex_web/site/welcome.cljs
+++ src/main/clojure/convex_web/site/welcome.cljs
@@ -1,16 +1,76 @@
 (ns convex-web.site.welcome
   (:require [convex-web.site.markdown :as markdown]
-            [convex-web.site.gui :as gui]))
+            [convex-web.site.gui :as gui]
+
+            [reagent.core :as reagent]
+            [reitit.frontend.easy :as rfe]))
 
 (defn Nav []
-  [:div.h-16.flex.justify-between
+  (let [state-ref (reagent/atom {})]
+    (fn []
+      [:div.h-16.flex.justify-between
 
-   [:div.flex.items-center
-    [gui/ConvexLogo {:width "28px" :height "32px"}]
-    [:span.font-mono.text-xl.ml-4.leading-none "Convex"]]
+       [:div.flex.items-center
+        [gui/ConvexLogo {:width "28px" :height "32px"}]
+        [:span.font-mono.text-xl.ml-4.leading-none "Convex"]]
 
-   [:div.flex
-    [:span "Guides"]]])
+       [:div.flex.items-center
+
+        ;; Dropdown
+        [:div.relative.inline-block.text-left
+
+         [:div
+
+          ;; Button
+          [:span.rounded-md.shadow-sm
+           [:button.inline-flex
+            {:class
+             "inline-flex justify-center
+              w-full
+              px-4 py-2
+              bg-white
+              text-sm
+              leading-5
+              font-medium text-gray-700 hover:text-gray-500
+              focus:outline-none focus:border-blue-300 focus:shadow-outline-blue
+              active:bg-gray-50 active:text-gray-800
+              transition ease-in-out duration-150"
+             :on-click #(swap! state-ref update :visible? not)}
+
+            "Guides"
+
+            [:svg.-mr-1.ml-2.h-5.w-5
+             {:viewBox "0 0 20 20"
+              :fill "currentColor"}
+             [:path
+              {:fill-rule "evenodd"
+               :d "M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+               :clip-rule "evenodd"}]]]]
+
+
+          [:div.origin-top-right.absolute.right-0.mt-2.w-56.rounded-md.shadow-lg
+           (if (:visible? @state-ref)
+             {:class ""}
+             {:class "hidden"})
+
+           [:div.rounded-md.bg-white.shadow-xs
+
+            [:div.py-1 {:role "menu" :aria-orientation "vertical" :aria-labelledby "options-menu"}
+
+             [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
+              {:href (rfe/href :route-name/documentation-concepts)}
+              "Concepts"]
+
+             [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
+              {:href (rfe/href :route-name/documentation-getting-started)}
+              "Getting Started"]
+
+             [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
+              {:href (rfe/href :route-name/documentation-tutorial)}
+              "Tutorial"]
+             ]]]]
+
+         ]]])))
 
 (defn WelcomePage [_ state _]
   [:div.w-full.max-w-screen-xl.mx-auto
