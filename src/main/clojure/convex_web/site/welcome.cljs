e42ffc26f1937f51ebf99c0849716c27a471bf3a
Dee Pheap <pheapdee35@gmail.com>
2020-09-03 18:16:09 +0700

    Guides dropdown with transition

--- src/main/clojure/convex_web/site/welcome.cljs
+++ src/main/clojure/convex_web/site/welcome.cljs
@@ -3,7 +3,9 @@
             [convex-web.site.gui :as gui]
 
             [reagent.core :as reagent]
-            [reitit.frontend.easy :as rfe]))
+            [reitit.frontend.easy :as rfe]
+
+            ["@tailwindui/react" :refer [Transition]]))
 
 (defn Nav []
   (let [state-ref (reagent/atom {})]
@@ -48,27 +50,33 @@
                :clip-rule "evenodd"}]]]]
 
 
-          [:div.origin-top-right.absolute.right-0.mt-2.w-56.rounded-md.shadow-lg
-           (if (:visible? @state-ref)
-             {:class ""}
-             {:class "hidden"})
+          [:> Transition
+           {:show (or (:visible? @state-ref) false)
+            :enter "transition ease-out duration-100"
+            :enterFrom "transform opacity-0 scale-95"
+            :enterTo "transform opacity-100 scale-100"
+            :leave "transition ease-in duration-75"
+            :leaveFrom "transform opacity-100 scale-100"
+            :leaveTo "transform opacity-0 scale-95"}
+           [:div.origin-top-right.absolute.right-0.mt-2.w-56.rounded-md.shadow-lg
 
-           [:div.rounded-md.bg-white.shadow-xs
+            [:div.rounded-md.bg-white.shadow-xs
 
-            [:div.py-1 {:role "menu" :aria-orientation "vertical" :aria-labelledby "options-menu"}
+             [:div.py-1 {:role "menu" :aria-orientation "vertical" :aria-labelledby "options-menu"}
 
-             [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
-              {:href (rfe/href :route-name/documentation-concepts)}
-              "Concepts"]
+              [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
+               {:href (rfe/href :route-name/documentation-concepts)}
+               "Concepts"]
 
-             [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
-              {:href (rfe/href :route-name/documentation-getting-started)}
-              "Getting Started"]
+              [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
+               {:href (rfe/href :route-name/documentation-getting-started)}
+               "Getting Started"]
 
-             [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
-              {:href (rfe/href :route-name/documentation-tutorial)}
-              "Tutorial"]
-             ]]]]
+              [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
+               {:href (rfe/href :route-name/documentation-tutorial)}
+               "Tutorial"]
+              ]]]
+           ]]
 
          ]]])))
 
