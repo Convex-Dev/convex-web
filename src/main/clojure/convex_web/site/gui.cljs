6985e5730456f2bb26ccc87da793fff141843eae
Dee Pheap <pheapdee35@gmail.com>
2020-09-04 10:30:00 +0700

    Nav WIP

--- src/main/clojure/convex_web/site/gui.cljs
+++ src/main/clojure/convex_web/site/gui.cljs
@@ -8,6 +8,8 @@
             ["react-tippy" :as tippy]
             ["react-markdown" :as ReactMarkdown]
 
+            ["@tailwindui/react" :refer [Transition]]
+
             [reagent.core :as reagent]
             [reitit.frontend.easy :as rfe]))
 
@@ -249,7 +251,7 @@
     [:button
      (merge {:class
              ["text-sm"
-              "px-2 py-1"
+              "px-4 py-2"
               "bg-gray-100"
               "rounded"
               "focus:outline-none"
@@ -475,3 +477,69 @@
    {:source markdown
     :renderers
     {:code (reagent/reactify-component MarkdownCodeBlock)}}])
+
+(defn DropdownButton [{:keys [text on-click]}]
+  [:button.inline-flex
+   {:class
+    "inline-flex justify-center
+     w-full
+     px-4 py-2
+     bg-white
+     leading-5
+     font-mono font-medium text-sm text-gray-700 hover:text-gray-500
+     focus:outline-none focus:border-blue-300 focus:shadow-outline-blue
+     active:bg-gray-50 active:text-gray-800
+     transition ease-in-out duration-150"
+    :on-click (or on-click identity)}
+
+   text
+
+   [:svg.-mr-1.ml-2.h-5.w-5
+    {:viewBox "0 0 20 20"
+     :fill "currentColor"}
+    [:path
+     {:fill-rule "evenodd"
+      :d "M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+      :clip-rule "evenodd"}]]])
+
+(defn DropdownNoBorder [{:keys [text]}]
+  (let [show? (reagent/atom false)]
+    (fn []
+      [:div.relative.inline-block.text-left
+
+       [:div
+
+        [DropdownButton
+         {:text text
+          :on-click #(swap! show? not)}]
+
+        ;; Choices
+        ;; ==============
+        [:> Transition
+         {:show @show?
+          :enter "transition ease-out duration-100"
+          :enterFrom "transform opacity-0 scale-95"
+          :enterTo "transform opacity-100 scale-100"
+          :leave "transition ease-in duration-75"
+          :leaveFrom "transform opacity-100 scale-100"
+          :leaveTo "transform opacity-0 scale-95"}
+         [:div.origin-top-right.absolute.right-0.mt-2.w-56.rounded-md.shadow-lg
+          [:div.rounded-md.bg-white.shadow-xs
+           [:div.py-1.font-mono
+            {:role "menu"
+             :aria-orientation "vertical"
+             :aria-labelledby "options-menu"}
+
+            [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
+             {:href (rfe/href :route-name/documentation-concepts)}
+             "Concepts"]
+
+            [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
+             {:href (rfe/href :route-name/documentation-getting-started)}
+             "Getting Started"]
+
+            [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
+             {:href (rfe/href :route-name/documentation-tutorial)}
+             "Tutorial"]
+
+            ]]]]]])))
--- src/main/clojure/convex_web/site/welcome.cljs
+++ src/main/clojure/convex_web/site/welcome.cljs
@@ -2,83 +2,35 @@
   (:require [convex-web.site.markdown :as markdown]
             [convex-web.site.gui :as gui]
 
-            [reagent.core :as reagent]
             [reitit.frontend.easy :as rfe]
 
             ["@tailwindui/react" :refer [Transition]]))
 
+(defn NavButton [text href]
+  [:a.font-mono.text-sm.hover:text-gray-500.px-4.py-2
+   {:href href}
+   text])
+
 (defn Nav []
-  (let [state-ref (reagent/atom {})]
-    (fn []
-      [:div.h-16.flex.justify-between
+  [:div.h-16.flex.justify-between
 
-       [:div.flex.items-center
-        [gui/ConvexLogo {:width "28px" :height "32px"}]
-        [:span.font-mono.text-xl.ml-4.leading-none "Convex"]]
+   ;; -- Logo
+   [:div.flex.items-center
+    [gui/ConvexLogo {:width "28px" :height "32px"}]
+    [:span.font-mono.text-xl.ml-4.leading-none "Convex"]]
 
-       [:div.flex.items-center
+   [:div.flex.items-center
+    ;; -- Guides
+    [gui/DropdownNoBorder {:text "Guides"}]
 
-        ;; Dropdown
-        [:div.relative.inline-block.text-left
+    ;; -- Sandbox
+    [NavButton "Sandbox" (rfe/href :route-name/sandbox)]
 
-         [:div
+    ;; -- Tools
+    [gui/DropdownNoBorder {:text "Tools"}]
 
-          ;; Button
-          [:span.rounded-md.shadow-sm
-           [:button.inline-flex
-            {:class
-             "inline-flex justify-center
-              w-full
-              px-4 py-2
-              bg-white
-              text-sm
-              leading-5
-              font-medium text-gray-700 hover:text-gray-500
-              focus:outline-none focus:border-blue-300 focus:shadow-outline-blue
-              active:bg-gray-50 active:text-gray-800
-              transition ease-in-out duration-150"
-             :on-click #(swap! state-ref update :visible? not)}
-
-            "Guides"
-
-            [:svg.-mr-1.ml-2.h-5.w-5
-             {:viewBox "0 0 20 20"
-              :fill "currentColor"}
-             [:path
-              {:fill-rule "evenodd"
-               :d "M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-               :clip-rule "evenodd"}]]]]
-
-
-          [:> Transition
-           {:show (or (:visible? @state-ref) false)
-            :enter "transition ease-out duration-100"
-            :enterFrom "transform opacity-0 scale-95"
-            :enterTo "transform opacity-100 scale-100"
-            :leave "transition ease-in duration-75"
-            :leaveFrom "transform opacity-100 scale-100"
-            :leaveTo "transform opacity-0 scale-95"}
-           [:div.origin-top-right.absolute.right-0.mt-2.w-56.rounded-md.shadow-lg
-
-            [:div.rounded-md.bg-white.shadow-xs
-
-             [:div.py-1 {:role "menu" :aria-orientation "vertical" :aria-labelledby "options-menu"}
-
-              [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
-               {:href (rfe/href :route-name/documentation-concepts)}
-               "Concepts"]
-
-              [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
-               {:href (rfe/href :route-name/documentation-getting-started)}
-               "Getting Started"]
-
-              [:a.block.px-4.py-2.text-sm.leading-5.text-gray-700.hover:bg-gray-100.hover:text-gray-900.focus:outline-none.focus:bg-gray-100.focus:text-gray-900
-               {:href (rfe/href :route-name/documentation-tutorial)}
-               "Tutorial"]
-              ]]]
-           ]]
-
-         ]]])))
+    ;; -- Explorer
+    [gui/DropdownNoBorder {:text "Explorer"}]]])
 
 (defn WelcomePage [_ state _]
   [:div.w-full.max-w-screen-xl.mx-auto
