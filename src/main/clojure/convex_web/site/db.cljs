178da7dca850dc139eff0ede061ad37f80dd12f5
Dee Pheap <pheapdee35@gmail.com>
2020-09-15 17:33:51 +0700

    Use event-fx for !transact to make the intent clearer

--- src/main/clojure/convex_web/site/db.cljs
+++ src/main/clojure/convex_web/site/db.cljs
@@ -2,9 +2,9 @@
   (:require [convex-web.site.runtime :refer [disp sub]]
             [re-frame.core :as re-frame]))
 
-(re-frame/reg-event-db :db/!transact
-  (fn [db [_ f args]]
-    (apply f db args)))
+(re-frame/reg-event-fx :db/!transact
+  (fn [{:keys [db]} [_ f args]]
+    {:db (apply f db args)}))
 
 (defn transact [f & args]
   (disp :db/!transact f args))
