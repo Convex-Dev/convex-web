b7c0c275ecc4e6d22a633b84770e7afe49087425
Dee Pheap <pheapdee35@gmail.com>
2020-09-28 13:17:50 +0700

    Make top nav edge to edge (like app pages)

--- src/main/clojure/convex_web/site/welcome.cljs
+++ src/main/clojure/convex_web/site/welcome.cljs
@@ -130,7 +130,7 @@
                 text])]]]]]]])))
 
 (defn Nav []
-  [:div.h-16.flex.items-center.justify-between.px-12
+  [:div.h-16.flex.items-center.justify-between.px-10
 
    ;; -- Logo
    [:a {:href (rfe/href :route-name/welcome)}
@@ -187,7 +187,7 @@
                [:div.flex.items-center
                 [gui/BulletIcon {:style {:min-width "40px" :min-height "40px"}}]
                 [:span.font-mono.ml-4 s]])]
-    [:div.w-full.max-w-screen-xl.mx-auto
+    [:div
 
      [Nav]
 
@@ -225,112 +225,116 @@
 
          [gui/ArrowCircleDownIcon {:class "h-4 w-4 text-black"}]]]]]
 
-     [:div#how.flex.flex-1.justify-center.my-14
-      [:span.inline-block.font-mono.text-center.text-4xl
-       {:class "w-4/5"
-        :style
-        {:color "#62A6E1"}}
-       "The tools to build the next generation of digital assets and applications are here."]]
+
+     [:div.w-full.max-w-screen-xl.mx-auto
+
+      [:div#how.flex.flex-1.justify-center.my-14
+       [:span.inline-block.font-mono.text-center.text-4xl
+        {:class "w-4/5"
+         :style
+         {:color "#62A6E1"}}
+        "The tools to build the next generation of digital assets and applications are here."]]
 
 
-     ;; Convex is flexible
-     ;; =========================
-     [:div.w-full.flex.mb-40.space-x-8
+      ;; Convex is flexible
+      ;; =========================
+      [:div.w-full.flex.mb-40.space-x-8
 
-      ;; -- Image
-      [:div {:class "w-1/2"}
-       [:img {:src "images/convex_flexible.png"}]]
+       ;; -- Image
+       [:div {:class "w-1/2"}
+        [:img {:src "images/convex_flexible.png"}]]
 
-      ;; -- Copy
-      [:div {:class marketing-vertical}
+       ;; -- Copy
+       [:div {:class marketing-vertical}
 
-       [:span.font-mono.text-4xl "Convex is Flexible"]
+        [:span.font-mono.text-4xl "Convex is Flexible"]
 
-       [:p.text-xl.leading-8
-        "Convex is well suited as a platform for applications that need to be
-         100% secure and also publicly verifiable (both in terms of data and
-         application behaviour), such as:"]
+        [:p.text-xl.leading-8
+         "Convex is well suited as a platform for applications that need to be
+          100% secure and also publicly verifiable (both in terms of data and
+          application behaviour), such as:"]
 
-       [:div {:class marketing-bullets}
-        [Item "Public registries and databases"]
-        [Item "Digital currencies"]
-        [Item "Prediction markets"]
-        [Item "Smart contracts for managing digital assets"]
-        [Item "Immutable provenance records"]]]]
-
-     ;; Convex is fast
-     ;; =========================
-     [:div.w-full.flex.mb-40.space-x-8
-
-      ;; -- Copy
-      [:div {:class marketing-vertical}
-
-       [:span.font-mono.text-4xl "Convex is Fast"]
-
-       [:p {:class marketing-copy}
-        "With a novel consensus algorithm, Convex is able to execute
-         decentralised applications at internet scale. Using normal consumer
-         grade hardware and network bandwidth the network can achieve:"]
-
-       [:div {:class marketing-bullets}
-        [Item
-         "Thousands of digitally signed transactions per second (more than the
-          1,700 transactions per second typically handled by the VISA network)"]
-
-        [Item
-         "Ability to execute over a million operations per second on the CVM"]
-
-        [Item
-         "Low latency (around 1 second for global consensus)"]]
-
-       [:p {:class marketing-copy}
-        "In the future, it will be possible to extend scalability even further
-         through proven techniques such as sharding, state channel or side
-         chains."]]
-
-      ;; -- Image
-      [:div {:class "w-1/2"}
-       [:img {:src "images/convex_fast.png"}]]]
+        [:div {:class marketing-bullets}
+         [Item "Public registries and databases"]
+         [Item "Digital currencies"]
+         [Item "Prediction markets"]
+         [Item "Smart contracts for managing digital assets"]
+         [Item "Immutable provenance records"]]]]
 
 
-     ;; Convex is fun
-     ;; =========================
-     [:div.w-full.flex.mb-40
+      ;; Convex is fast
+      ;; =========================
+      [:div.w-full.flex.mb-40.space-x-8
 
-      ;; -- Image
-      [:div {:class "w-1/2"}
-       [:img {:src "images/convex_fun.png"}]]
+       ;; -- Copy
+       [:div {:class marketing-vertical}
 
-      ;; -- Copy
-      [:div {:class marketing-vertical}
+        [:span.font-mono.text-4xl "Convex is Fast"]
 
-       [:span.font-mono.text-4xl "Convex is Fun"]
+        [:p {:class marketing-copy}
+         "With a novel consensus algorithm, Convex is able to execute
+          decentralised applications at internet scale. Using normal consumer
+          grade hardware and network bandwidth the network can achieve:"]
 
-       [:p {:class marketing-copy}
-        "We believe in providing a powerful, interactive environment for
-         development in Convex that enables high productivity while maintaining
-         secure coding principles."]
+        [:div {:class marketing-bullets}
+         [Item
+          "Thousands of digitally signed transactions per second (more than the
+           1,700 transactions per second typically handled by the VISA network)"]
 
-       [:p {:class marketing-copy}
-        "Convex provides an interactive REPL allowing users to code directly
-         on the Convex platform using Convex Lisp."]
+         [Item
+          "Ability to execute over a million operations per second on the CVM"]
 
-       [:a
-        {:href (rfe/href :route-name/documentation-getting-started)}
-        [gui/SecondaryButton
-         {}
-         [:span.block.font-mono.text-sm.text-white.uppercase
-          {:class gui/button-child-large-padding}
-          "Try It For Yourself"]]]]]
+         [Item
+          "Low latency (around 1 second for global consensus)"]]
+
+        [:p {:class marketing-copy}
+         "In the future, it will be possible to extend scalability even further
+          through proven techniques such as sharding, state channel or side
+          chains."]]
+
+       ;; -- Image
+       [:div {:class "w-1/2"}
+        [:img {:src "images/convex_fast.png"}]]]
+
+      ;; Convex is fun
+      ;; =========================
+      [:div.w-full.flex.mb-40
+
+       ;; -- Image
+       [:div {:class "w-1/2"}
+        [:img {:src "images/convex_fun.png"}]]
+
+       ;; -- Copy
+       [:div {:class marketing-vertical}
+
+        [:span.font-mono.text-4xl "Convex is Fun"]
+
+        [:p {:class marketing-copy}
+         "We believe in providing a powerful, interactive environment for
+          development in Convex that enables high productivity while maintaining
+          secure coding principles."]
+
+        [:p {:class marketing-copy}
+         "Convex provides an interactive REPL allowing users to code directly
+          on the Convex platform using Convex Lisp."]
+
+        [:a
+         {:href (rfe/href :route-name/documentation-getting-started)}
+         [gui/SecondaryButton
+          {}
+          [:span.block.font-mono.text-sm.text-white.uppercase
+           {:class gui/button-child-large-padding}
+           "Try It For Yourself"]]]]]
 
 
-     ;; Bottom nav
-     ;; =========================
-     [:div.mb-20
-      [BottomNav]]
+      ;; Bottom nav
+      ;; =========================
+      [:div.mb-20
+       [BottomNav]]]
 
 
      [:hr.border-gray-200.mb-8]
+
 
      ;; Copyright
      ;; =========================
