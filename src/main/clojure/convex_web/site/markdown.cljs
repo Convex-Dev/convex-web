68ea9f461a0fcdcc979da6b72f575f3d1b0b3b58
Dee Pheap <pheapdee35@gmail.com>
2020-09-30 14:53:18 +0700

    Refactor Markdown page

--- src/main/clojure/convex_web/site/markdown.cljs
+++ src/main/clojure/convex_web/site/markdown.cljs
@@ -3,87 +3,65 @@
             [convex-web.site.backend :as backend]
             [reagent.core :as reagent]))
 
-(defn get-on-push
-  "Returns a function to get markdown from the server.
+(defn MarkdownPage [_ {:keys [markdown]} _]
+  (reagent/with-let [*nodes (reagent/atom nil)]
+    (let [{:keys [ajax/status contents toc? smart-toc?] :or {toc? true}} markdown]
+      [:div.flex.flex-1.overflow-auto
+       (case status
+         :ajax.status/pending
+         [:div.flex.flex-1.items-center.justify-center
+          [gui/Spinner]]
 
-   Markdown state will be stored under the key `:markdown`."
-  [markdown-key]
-  (fn [_ _ set-state]
-    (set-state update :markdown assoc :ajax/status :ajax.status/pending)
+         :ajax.status/error
+         [:span "Error"]
 
-    (backend/GET-markdown-page
-      markdown-key
-      {:handler
-       (fn [markdown-page]
-         (set-state update :markdown merge {:ajax/status :ajax.status/success} markdown-page))
+         :ajax.status/success
+         [:<>
+          ;; -- Markdown
+          [:div.overflow-auto
+           {:ref
+            (fn [el]
+              (when (and el smart-toc?)
+                (reset! *nodes (.querySelectorAll el "h2"))))
 
-       :error-handler
-       (fn [error]
-         (set-state update :markdown assoc :ajax/status :ajax.status/error :ajax/error error))})))
+            :class
+            (if toc?
+              ;; Add right padding to have some spacing between the markdown and the TOC.
+              "pr-10"
+              ;; Without a TOC the markdown must have a full width.
+              "flex-1")}
 
-(defn Markdown [{:keys [markdown]}]
-  (let [*nodes (reagent/atom nil)]
-    (fn [{:keys [markdown]}]
-      (let [{:keys [ajax/status contents toc? smart-toc?] :or {toc? true}} markdown]
-        [:div.flex.flex-1.overflow-auto
-         (case status
-           :ajax.status/pending
-           [:div.flex.flex-1.items-center.justify-center
-            [gui/Spinner]]
+           (for [{:keys [name content]} contents]
+             ^{:key name}
+             [:article.prose.mb-10
+              {:id name}
+              [gui/Markdown content]])]
 
-           :ajax.status/error
-           [:span "Error"]
+          ;; -- On this page
+          (when toc?
+            (let [item-style "text-gray-600 hover:text-gray-900 cursor-pointer"]
+              [:div.flex.flex-col.ml-10
+               [:span.text-xs.text-gray-500.font-bold.uppercase "On this Page"]
 
-           :ajax.status/success
-           [:<>
-            ;; -- Markdown
-            [:div.overflow-auto
-             {:ref
-              (fn [el]
-                (when (and el smart-toc?)
-                  (reset! *nodes (.querySelectorAll el "h2"))))
+               [:ul.list-none.text-sm.mt-4.space-y-2
+                (if smart-toc?
+                  ;; Smart ToC
+                  (for [node @*nodes]
+                    ^{:key (.-textContent node)}
+                    [:li.mb-2
+                     [:a.text-gray-600.hover:text-gray-900.cursor-pointer
+                      {:on-click #(gui/scroll-element-into-view node)}
+                      (.-textContent node)]])
 
-              :class
-              (if toc?
-                ;; Add right padding to have some spacing between the markdown and the TOC.
-                "pr-10"
-                ;; Without a TOC the markdown must have a full width.
-                "flex-1")}
+                  ;; Default ToC
+                  (for [{:keys [name]} contents]
+                    ^{:key name}
+                    [:li
+                     {:class item-style
+                      :on-click #(gui/scroll-into-view name)}
+                     name]))]]))]
 
-             (for [{:keys [name content]} contents]
-               ^{:key name}
-               [:article.prose.mb-10
-                {:id name}
-                [gui/Markdown content]])]
-
-            ;; -- On this page
-            (when toc?
-              (let [item-style "text-gray-600 hover:text-gray-900 cursor-pointer"]
-                [:div.flex.flex-col.ml-10
-                 [:span.text-xs.text-gray-500.font-bold.uppercase "On this Page"]
-
-                 [:ul.list-none.text-sm.mt-4.space-y-2
-                  (if smart-toc?
-                    ;; Smart ToC
-                    (for [node @*nodes]
-                      ^{:key (.-textContent node)}
-                      [:li.mb-2
-                       [:a.text-gray-600.hover:text-gray-900.cursor-pointer
-                        {:on-click #(gui/scroll-element-into-view node)}
-                        (.-textContent node)]])
-
-                    ;; Default ToC
-                    (for [{:keys [name]} contents]
-                      ^{:key name}
-                      [:li
-                       {:class item-style
-                        :on-click #(gui/scroll-into-view name)}
-                       name]))]]))]
-
-           [:div])]))))
-
-(defn MarkdownPage [_ state _]
-  [Markdown state])
+         [:div])])))
 
 (def markdown-page
   #:page {:id :page.id/markdown
