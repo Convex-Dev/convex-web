2a27f81144f3c4049db0b444216bf7f5d8eb13f5
Dee Pheap <pheapdee35@gmail.com>
2020-10-07 15:42:39 +0700

    Encoding docstrings

--- src/main/clojure/convex_web/encoding.clj
+++ src/main/clojure/convex_web/encoding.clj
@@ -3,14 +3,20 @@
             [clojure.java.io :as io])
   (:import (java.io ByteArrayOutputStream InputStream)))
 
-(defn transit-decode-string [^String s]
+(defn transit-decode-string
+  "Decode a Transit-JSON encoded string `s`."
+  [^String s]
   (t/read (t/reader (io/input-stream (.getBytes s)) :json)))
 
 (defn transit-decode [^InputStream x]
   (when x
     (t/read (t/reader x :json))))
 
-(defn transit-encode [x]
+(defn transit-encode
+  "Encode `x` in Transit-JSON; or as `(str x)` if the encoding fails.
+
+   Returns a Transit JSON-encoded string."
+  [x]
   (try
     (let [out (ByteArrayOutputStream. 4096)
           writer (t/writer out :json)]
