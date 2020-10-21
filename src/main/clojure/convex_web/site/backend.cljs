bf1b95a18794467dc264c35974ba181eff434261
Dee Pheap <pheapdee35@gmail.com>
2020-10-21 11:41:07 +0700

    Fix routes - prefix '/'

--- src/main/clojure/convex_web/site/backend.cljs
+++ src/main/clojure/convex_web/site/backend.cljs
@@ -5,9 +5,9 @@
   {"x-csrf-token" (.-value (.getElementById js/document "__anti-forgery-token"))})
 
 (defn GET-session [{:keys [handler error-handler]}]
-  (GET "api/internal/session" (merge {:handler handler}
-                                     (when error-handler
-                                       {:error-handler error-handler}))))
+  (GET "/api/internal/session" (merge {:handler handler}
+                                      (when error-handler
+                                        {:error-handler error-handler}))))
 
 (defn GET-accounts
   "Gets a range of Accounts."
@@ -16,23 +16,23 @@
                         {:start start})
                       (when end
                         {:end end}))]
-    (GET "api/internal/accounts" (merge {:handler handler}
-                                        (when error-handler
-                                          {:error-handler error-handler})
-                                        (when params
-                                          {:params params})))))
+    (GET "/api/internal/accounts" (merge {:handler handler}
+                                         (when error-handler
+                                           {:error-handler error-handler})
+                                         (when params
+                                           {:params params})))))
 
 (defn GET-account [address {:keys [handler error-handler]}]
-  (GET (str "api/internal/accounts/" address) (merge {:handler handler}
-                                                     (when error-handler
-                                                       {:error-handler error-handler}))))
+  (GET (str "/api/internal/accounts/" address) (merge {:handler handler}
+                                                      (when error-handler
+                                                        {:error-handler error-handler}))))
 
 (defn GET-blocks
   "Gets the latest `n` Blocks, or start from `i` and take `n`."
   [{:keys [handler error-handler]}]
-  (GET "api/internal/blocks" (merge {:handler handler}
-                                    (when error-handler
-                                      {:error-handler error-handler}))))
+  (GET "/api/internal/blocks" (merge {:handler handler}
+                                     (when error-handler
+                                       {:error-handler error-handler}))))
 
 (defn GET-blocks-range
   "Gets a range of Blocks."
@@ -41,70 +41,70 @@
                         {:start start})
                       (when end
                         {:end end}))]
-    (GET "api/internal/blocks-range" (merge {:handler handler}
-                                            (when params
-                                              {:params params})
-                                            (when error-handler
-                                              {:error-handler error-handler})))))
+    (GET "/api/internal/blocks-range" (merge {:handler handler}
+                                             (when params
+                                               {:params params})
+                                             (when error-handler
+                                               {:error-handler error-handler})))))
 
 (defn GET-block [index {:keys [handler error-handler]}]
-  (GET (str "api/internal/blocks/" index) (merge {:handler handler}
-                                                 (when error-handler
-                                                   {:error-handler error-handler}))))
+  (GET (str "/api/internal/blocks/" index) (merge {:handler handler}
+                                                  (when error-handler
+                                                    {:error-handler error-handler}))))
 
 (defn POST-generate-account [{:keys [handler error-handler]}]
-  (POST "api/internal/generate-account" (merge {:headers (csrf-header)
-                                                :handler handler}
-                                               (when error-handler
-                                                 {:error-handler error-handler}))))
-
-(defn POST-confirm-account [address {:keys [handler error-handler]}]
-  (POST "api/internal/confirm-account" (merge {:headers (csrf-header)
-                                               :handler handler
-                                               :params address}
-                                              (when error-handler
-                                                {:error-handler error-handler}))))
-
-(defn POST-command [command {:keys [handler error-handler]}]
-  (POST "api/internal/commands" (merge {:headers (csrf-header)
-                                        :handler handler
-                                        :params command}
-                                       (when error-handler
-                                         {:error-handler error-handler}))))
-
-(defn GET-command [id {:keys [handler error-handler]}]
-  (GET (str "api/internal/commands/" id) (merge {:handler handler}
+  (POST "/api/internal/generate-account" (merge {:headers (csrf-header)
+                                                 :handler handler}
                                                 (when error-handler
                                                   {:error-handler error-handler}))))
 
+(defn POST-confirm-account [address {:keys [handler error-handler]}]
+  (POST "/api/internal/confirm-account" (merge {:headers (csrf-header)
+                                                :handler handler
+                                                :params address}
+                                               (when error-handler
+                                                 {:error-handler error-handler}))))
+
+(defn POST-command [command {:keys [handler error-handler]}]
+  (POST "/api/internal/commands" (merge {:headers (csrf-header)
+                                         :handler handler
+                                         :params command}
+                                        (when error-handler
+                                          {:error-handler error-handler}))))
+
+(defn GET-command [id {:keys [handler error-handler]}]
+  (GET (str "/api/internal/commands/" id) (merge {:handler handler}
+                                                 (when error-handler
+                                                   {:error-handler error-handler}))))
+
 (defn POST-faucet [faucet {:keys [handler error-handler]}]
-  (POST "api/internal/faucet" (merge {:headers (csrf-header)
-                                      :handler handler
-                                      :params faucet}
-                                     (when error-handler
-                                       {:error-handler error-handler}))))
+  (POST "/api/internal/faucet" (merge {:headers (csrf-header)
+                                       :handler handler
+                                       :params faucet}
+                                      (when error-handler
+                                        {:error-handler error-handler}))))
 
 (defn GET-reference [{:keys [handler error-handler]}]
-  (GET "api/internal/reference" (merge {:handler handler}
-                                       (when error-handler
-                                         {:error-handler error-handler}))))
+  (GET "/api/internal/reference" (merge {:handler handler}
+                                        (when error-handler
+                                          {:error-handler error-handler}))))
 
 (defn GET-markdown-page [k {:keys [handler error-handler]}]
-  (GET "api/internal/markdown-page" (merge {:params {"page" k}
-                                            :handler handler}
-                                           (when error-handler
-                                             {:error-handler error-handler}))))
+  (GET "/api/internal/markdown-page" (merge {:params {"page" k}
+                                             :handler handler}
+                                            (when error-handler
+                                              {:error-handler error-handler}))))
 
 (defn POST-transaction-prepare [{:keys [address source] :as params} {:keys [handler error-handler]}]
-  (POST "api/v1/transaction/prepare" (merge {:handler handler
+  (POST "/api/v1/transaction/prepare" (merge {:handler handler
+                                              :format :json
+                                              :params params}
+                                             (when error-handler
+                                               {:error-handler error-handler}))))
+
+(defn POST-transaction-submit [{:keys [hash] :as params} {:keys [handler error-handler]}]
+  (POST "/api/v1/transaction/submit" (merge {:handler handler
                                              :format :json
                                              :params params}
                                             (when error-handler
                                               {:error-handler error-handler}))))
-
-(defn POST-transaction-submit [{:keys [hash] :as params} {:keys [handler error-handler]}]
-  (POST "api/v1/transaction/submit" (merge {:handler handler
-                                            :format :json
-                                            :params params}
-                                           (when error-handler
-                                             {:error-handler error-handler}))))
