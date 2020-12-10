a2c3de9ec9362d2a64e002e0e8732b5f2a021299
Dee Pheap <pheapdee35@gmail.com>
2020-12-10 16:20:34 +0700

    Minor tweak to shutdown hook

--- src/main/clojure/convex_web/core.clj
+++ src/main/clojure/convex_web/core.clj
@@ -6,15 +6,18 @@
 (defn -main
   "Start Convex Web."
   [& _]
-  (let [_ (log/info "Starting system...")
+  (let [system (do
+                 (log/info "Starting system...")
+                 (com.stuartsierra.component/start (convex-web.component/system :prod)))
 
-        system (com.stuartsierra.component/start (convex-web.component/system :prod))]
+        shutdown-hook (Thread. ^Runnable
+                               (fn []
+                                 (try
+                                   (log/info "Stopping system...")
 
-    (.addShutdownHook (Runtime/getRuntime) (Thread. ^Runnable (fn []
-                                                                (try
-                                                                  (log/info "Stopping system...")
+                                   (com.stuartsierra.component/stop system)
 
-                                                                  (com.stuartsierra.component/stop system)
+                                   (catch Exception ex
+                                     (log/error ex "There was an error while stopping system in shutdown hook.")))))]
 
-                                                                  (catch Exception ex
-                                                                    (log/error ex "There was an error while stopping system in shutdown hook."))))))))
+    (.addShutdownHook (Runtime/getRuntime) shutdown-hook)))
