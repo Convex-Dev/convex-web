d6b028645f7a2860a625b02c3dfc3ccfc7700d17
Pedro Girardi <pedro@plando.work>
2021-06-09 15:21:32 +0700

    Reformat

--- src/test/clojure/convex_web/convex_test.clj
+++ src/test/clojure/convex_web/convex_test.clj
@@ -55,69 +55,69 @@
 (deftest datafy-test
   (let [context (make-convex-context)]
     (testing "nil" (is (= nil (convex/datafy nil))))
-
+    
     (testing "Byte"
-             (is (= convex.core.data.prim.CVMByte
-                    (type (convex/execute-string context "(nth 0xFF 0)"))))
-             (is (= 255
-                    (convex/datafy (convex/execute-string context
-                                                          "(nth 0xFF 0)")))))
-
-    (testing "Char" (is (= \n (convex/datafy (convex/execute context \n)))))
-
-    (testing "Long" (is (= 1 (convex/datafy (convex/execute context 1)))))
-
-    (testing "Double" (is (= 1.0 (convex/datafy (convex/execute context 1.0)))))
-
-    (testing "Keyword" (is (= :a (convex/datafy (convex/execute context :a)))))
-
+      (is (= convex.core.data.prim.CVMByte (type (convex/execute-string context "(nth 0xFF 0)"))))
+      (is (= 255 (convex/datafy (convex/execute-string context "(nth 0xFF 0)")))))
+    
+    (testing "Char" 
+      (is (= \n (convex/datafy (convex/execute context \n)))))
+    
+    (testing "Long" 
+      (is (= 1 (convex/datafy (convex/execute context 1)))))
+    
+    (testing "Double" 
+      (is (= 1.0 (convex/datafy (convex/execute context 1.0)))))
+    
+    (testing "Keyword" 
+      (is (= :a (convex/datafy (convex/execute context :a)))))
+    
     (testing "Symbol"
-             (is (= 's (convex/datafy (convex/execute-string context "'s"))))
-             (is (= '(lookup a b)
-                    (convex/datafy (convex/execute-string context "'a/b"))))
-             (is (= '(lookup 8 inc)
-                    (convex/datafy (convex/execute-string context "'#8/inc")))))
-
-    (testing "List" (is (= '() (convex/datafy (convex/execute context '())))))
-
-    (testing "Vector" (is (= [] (convex/datafy (convex/execute context [])))))
-
+      (is (= 's (convex/datafy (convex/execute-string context "'s"))))
+      (is (= '(lookup a b) (convex/datafy (convex/execute-string context "'a/b"))))
+      (is (= '(lookup 8 inc) (convex/datafy (convex/execute-string context "'#8/inc")))))
+    
+    (testing "List" 
+      (is (= '() (convex/datafy (convex/execute context '())))))
+    
+    (testing "Vector" 
+      (is (= [] (convex/datafy (convex/execute context [])))))
+    
     (testing "Map"
-             (is (= {} (convex/datafy (convex/execute context {}))))
-             (is (= {'(1) '(2)}
-                    (convex/datafy (convex/execute context
-                                                   {(list 1) (list 2)})))))
-
-    (testing "Set" (is (= #{} (convex/datafy (convex/execute context #{})))))
-
+      (is (= {} (convex/datafy (convex/execute context {}))))
+      (is (= {'(1) '(2)} (convex/datafy (convex/execute context {(list 1) (list 2)})))))
+    
+    (testing "Set" 
+      (is (= #{} (convex/datafy (convex/execute context #{})))))
+    
     (testing "AccountKey"
-             (is (= (-> Init/HERO_KP
-                        .getAccountKey
-                        .toChecksumHex)
-                    (convex/datafy (.getAccountKey Init/HERO_KP)))))
-
+      (is (= (-> Init/HERO_KP
+               .getAccountKey
+               .toChecksumHex)
+            (convex/datafy (.getAccountKey Init/HERO_KP)))))
+    
     (testing "Address"
-             (is (= (.longValue Init/HERO) (convex/datafy Init/HERO))))
-
+      (is (= (.longValue Init/HERO) (convex/datafy Init/HERO))))
+    
     (testing "Blob"
-             (is (= (.toHexString (Blob/create (.getBytes "Text")))
-                    (convex/datafy (Blob/create (.getBytes "Text"))))))
-
+      (is (= (.toHexString (Blob/create (.getBytes "Text")))
+            (convex/datafy (Blob/create (.getBytes "Text"))))))
+    
     (testing "Syntax"
-             (is (= 1 (convex/datafy (Syntax/create (CVMLong/create 1)))))
-             (is (= (Maps/empty) (convex/datafy (Syntax/create (Maps/empty))))))
-
+      (is (= 1 (convex/datafy (Syntax/create (CVMLong/create 1)))))
+      (is (= (Maps/empty) (convex/datafy (Syntax/create (Maps/empty))))))
+    
     (testing "Non-CVM types will throw"
-             (is (= "Can't datafy :x clojure.lang.Keyword."
-                    (ex-message (catch-throwable (convex/datafy :x)))))
-             (is (= "Can't datafy () clojure.lang.PersistentList$EmptyList."
-                    (ex-message (catch-throwable (convex/datafy '())))))
-             (is (= "Can't datafy 1 java.lang.Long."
-                    (ex-message (catch-throwable (convex/datafy 1)))))
-             (is (= "Can't datafy 1.0 java.lang.Double."
-                    (ex-message (catch-throwable (convex/datafy 1.0)))))
-             (is (= "Can't datafy \"ABC\" java.lang.String."
-                    (ex-message (catch-throwable (convex/datafy "ABC"))))))))
+      (is (= "Can't datafy :x clojure.lang.Keyword."
+            (ex-message (catch-throwable (convex/datafy :x)))))
+      (is (= "Can't datafy () clojure.lang.PersistentList$EmptyList."
+            (ex-message (catch-throwable (convex/datafy '())))))
+      (is (= "Can't datafy 1 java.lang.Long."
+            (ex-message (catch-throwable (convex/datafy 1)))))
+      (is (= "Can't datafy 1.0 java.lang.Double."
+            (ex-message (catch-throwable (convex/datafy 1.0)))))
+      (is (= "Can't datafy \"ABC\" java.lang.String."
+            (ex-message (catch-throwable (convex/datafy "ABC"))))))))
 
 (deftest address-test
   (testing "Coerce string to Address"
