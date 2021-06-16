9e496109a380b4e2ff88ebd59fbf29bc9867c90e
Pedro Girardi <pedro@plando.work>
2021-06-16 14:59:41 +0700

    Select library

--- src/main/clojure/convex_web/site/documentation.cljs
+++ src/main/clojure/convex_web/site/documentation.cljs
@@ -5,59 +5,74 @@
             [clojure.string :as str]
             [cljs.spec.alpha :as s]))
 
-(defn ReferencePage [_ {:keys [ajax/status reference] :as state} _]
+(defn ReferencePage [_ {:keys [ajax/status selected-library reference] :as state} set-state]
   (case status
     :ajax.status/pending
     [:div.flex.flex-1.items-center.justify-center
      [gui/Spinner]]
-
+    
     :ajax.status/error
     [:div.flex.flex-1.items-center.justify-center
      [:span "Sorry. Our servers failed to process your request."]]
-
+    
     :ajax.status/success
-    [:div.flex.flex-1.overflow-auto
-
-     ;; Documentation
-     [:div.flex.flex-col.overflow-auto {:class "w-1/2"}
-      (for [metadata reference]
-        (let [symbol (get-in metadata [:doc :symbol])]
-          ^{:key symbol}
-          [:div.flex.flex-col.flex-1
-           {:id (str "ref-" symbol)
-            :ref (fn [el]
-                   (when (and el (= symbol (:symbol state)))
-                     (.scrollIntoView el)))}
-           [gui/SymbolMeta metadata]
-
-           [:hr.my-2]]))]
-
-     ;; TOC
-     [:div.ml-16.mb-16.bg-yellow-100.rounded.shadow.overflow-auto
-      {:class "w-1/2"
-       :style {:min-width "220px"}}
-      [:div.flex.flex-col.leading-snug.p-4
-       (let [reference-grouped-by (->> reference
-                                       (group-by
-                                         (fn [metadata]
-                                           (let [[c] (get-in metadata [:doc :symbol])]
-                                             (if (re-matches #"^\w" c)
-                                               (str/upper-case c)
-                                               "*"))))
-                                       (sort-by first))]
-
-         (for [[k metas] reference-grouped-by]
-           ^{:key k}
-           [:div.flex.flex-wrap.items-end.mb-1
-            [:code.text-blue-500.text-xs.font-bold.mr-2 k]
-            (for [meta metas]
-              (let [symbol (get-in meta [:doc :symbol])]
-                ^{:key symbol}
-                [:a.mr-2
-                 {:on-click #(some-> (.getElementById js/document (str "ref-" symbol))
-                                     (.scrollIntoView))}
-                 [:code.text-xs.hover:underline.cursor-pointer symbol]]))]))]]]
-
+    [:div.flex.flex-col.flex-1.space-y-3.overflow-auto
+     
+     ;; Libraries.
+     (let [libraries ["convex.core" "convex.trust" "convex.registry"]]
+       [:div.flex.space-x-4
+        (for [library-name libraries]
+          ^{:key library-name}
+          [:span.font-mono.text-sm.cursor-pointer
+           {:class (if (= library-name selected-library)
+                     "text-blue-500 font-bold"
+                     "text-gray-600")
+            :on-click #(set-state assoc :selected-library library-name)}
+           library-name])])
+     
+     ;; Content.
+     [:div.flex.flex-1.overflow-auto
+      
+      ;; Documentation
+      [:div.flex.flex-col.overflow-auto {:class "w-1/2"}
+       (for [metadata reference]
+         (let [symbol (get-in metadata [:doc :symbol])]
+           ^{:key symbol}
+           [:div.flex.flex-col.flex-1
+            {:id (str "ref-" symbol)
+             :ref (fn [el]
+                    (when (and el (= symbol (:symbol state)))
+                      (.scrollIntoView el)))}
+            [gui/SymbolMeta metadata]
+            
+            [:hr.my-2]]))]
+      
+      ;; TOC
+      [:div.ml-16.mb-16.bg-yellow-100.rounded.shadow.overflow-auto
+       {:class "w-1/2"
+        :style {:min-width "220px"}}
+       [:div.flex.flex-col.leading-snug.p-4
+        (let [reference-grouped-by (->> reference
+                                     (group-by
+                                       (fn [metadata]
+                                         (let [[c] (get-in metadata [:doc :symbol])]
+                                           (if (re-matches #"^\w" c)
+                                             (str/upper-case c)
+                                             "*"))))
+                                     (sort-by first))]
+          
+          (for [[k metas] reference-grouped-by]
+            ^{:key k}
+            [:div.flex.flex-wrap.items-end.mb-1
+             [:code.text-blue-500.text-xs.font-bold.mr-2 k]
+             (for [meta metas]
+               (let [symbol (get-in meta [:doc :symbol])]
+                 ^{:key symbol}
+                 [:a.mr-2
+                  {:on-click #(some-> (.getElementById js/document (str "ref-" symbol))
+                                (.scrollIntoView))}
+                  [:code.text-xs.hover:underline.cursor-pointer symbol]]))]))]]]]
+    
     [:div]))
 
 (s/def :reference-page.state/symbol :convex-web/non-empty-string)
@@ -71,6 +86,7 @@
           (s/nilable
             (s/keys :opt-un [:reference-page.state/symbol
                              :reference-page.state/reference]))
+          :initial-state {:selected-library "convex.core"}
           :on-push
           (fn [_ _ set-state]
             (set-state assoc :ajax/status :ajax.status/pending)
