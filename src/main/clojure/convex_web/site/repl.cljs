82ab2d3dcafcfa0e59a639c2bab85ba15c38834b
Pedro Girardi <pedro@plando.work>
2021-06-17 14:42:46 +0700

    Fix unused binding warning

--- src/main/clojure/convex_web/site/repl.cljs
+++ src/main/clojure/convex_web/site/repl.cljs
@@ -277,7 +277,7 @@
               [:path {:fill-rule "evenodd", :d "M3 7a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 13a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z", :clip-rule "evenodd"}]]])
           :resizeHandles #js ["n"]
           :onResizeStop
-          (fn [event data]
+          (fn [_ _]
             ;; Move focus to editor.
             (codemirror/cm-focus @editor-ref))}
          
