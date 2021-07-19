e0a03d89ecb6a05f25828e791f956db3eafd0680
Pedro Girardi <pedro@plando.work>
2021-07-19 15:13:25 +0700

    Set default UncaughtExceptionHandler

--- src/main/clojure/convex_web/core.clj
+++ src/main/clojure/convex_web/core.clj
@@ -9,15 +9,20 @@
   (let [system (do
                  (log/info "Starting system...")
                  (com.stuartsierra.component/start (convex-web.component/system :prod)))
-
-        shutdown-hook (Thread. ^Runnable
-                               (fn []
-                                 (try
-                                   (log/info "Stopping system...")
-
-                                   (com.stuartsierra.component/stop system)
-
-                                   (catch Exception ex
-                                     (log/error ex "There was an error while stopping system in shutdown hook.")))))]
-
-    (.addShutdownHook (Runtime/getRuntime) shutdown-hook)))
+        
+        shutdown-hook (Thread. ^Runnable (fn []
+                                           (try
+                                             (log/info "Stopping system...")
+                                             
+                                             (com.stuartsierra.component/stop system)
+                                             
+                                             (catch Exception ex
+                                               (log/error ex "There was an error while stopping system in shutdown hook.")))))]
+    
+    (.addShutdownHook (Runtime/getRuntime) shutdown-hook)
+    
+    (Thread/setDefaultUncaughtExceptionHandler
+      (reify
+        Thread$UncaughtExceptionHandler
+        (uncaughtException [_ thread throwable]
+          (log/error throwable (format "Uncaught exception %s on thread %s" throwable thread)))))))
