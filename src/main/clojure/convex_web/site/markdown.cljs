3008d2a3f25ff949dbf759e0a9220b138075e275
Pedro Girardi <pedro@plando.work>
2021-08-02 17:16:28 +0700

    Markdown page - Tweak horizontal spacing

--- src/main/clojure/convex_web/site/markdown.cljs
+++ src/main/clojure/convex_web/site/markdown.cljs
@@ -18,16 +18,11 @@
          :ajax.status/success
          [:<>
           ;; -- Markdown
-          [:div.flex-1
+          [:div
            {:ref
             (fn [el]
               (when (and el smart-toc?)
-                (reset! *nodes (.querySelectorAll el "h2"))))
-
-            :class
-            (when toc?
-              ;; Without a TOC the markdown must have a full width.
-              "flex-1")}
+                (reset! *nodes (.querySelectorAll el "h2"))))}
 
            (for [{:keys [name content]} contents]
              ^{:key name}
@@ -38,7 +33,7 @@
           ;; -- On this page
           (when toc?
             (let [item-style "text-gray-600 hover:text-gray-900 cursor-pointer"]
-              [:div.hidden.md:flex.md:flex-col.md:mx-10
+              [:div.hidden.md:flex.md:flex-col.md:mr-10.ml-16
                [:span.text-xs.text-gray-500.font-bold.uppercase "On this Page"]
 
                [:ul.list-none.text-sm.mt-4.space-y-2.overflow-auto
