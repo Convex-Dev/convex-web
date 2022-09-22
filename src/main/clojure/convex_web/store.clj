4c71186ce0b84db9b72345d10d699b15d45474a6
Adam Helins <adam@protosens.com>
2022-09-22 19:16:49 +0200

    Refactor `convex-web.store`

--- src/main/clojure/convex_web/store.clj
+++ src/main/clojure/convex_web/store.clj
@@ -1,35 +1,47 @@
 (ns convex-web.store
-  (:require [clojure.java.io :as io]
-            [clojure.tools.logging :as log])
-  (:import (etch EtchStore)
-           (convex.core.store Stores)))
 
-(def store nil)
+  "Preparing Etch instances."
 
-(defn ^EtchStore create! [{:keys [etch-store-temp?
-                                  etch-store-temp-prefix
-                                  etch-store] :as config}]
+  (:require [clojure.java.io       :as java.io]
+            [clojure.tools.logging :as log]
+            [convex.db             :as $.db]))
+
+
+(set! *warn-on-reflection*
+      true)
+
+
+;;;;;;;;;;
+
+
+(defn create!
+
+  "Creates an Etch instance and sets it for global use."
+  
+  [{:as   config
+    :keys [etch-store
+           etch-store-temp-prefix
+           etch-store-temp?]}]
+
   (try
-    (or store 
-      (let [^EtchStore store (if etch-store-temp?
-                               (EtchStore/createTemp etch-store-temp-prefix)
-                               (let [etch-store-file (io/file etch-store)]
-                                 ;; Creates the directory named by this abstract pathname, 
-                                 ;; including any necessary but nonexistent parent directories.
-                                 ;; https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/io/File.html
-                                 (when-let [parent (.getParentFile etch-store-file)]
-                                   (when-not (.exists parent)
-                                     (.mkdirs parent)))
-                                 
-                                 (EtchStore/create etch-store-file)))]
-        
-        (Stores/setGlobalStore store)
-        
-        (alter-var-root #'store (constantly store))
-        
-        (log/debug "Store is ready" store)
-        
-        store))
+    ;;
+    (let [store (if etch-store-temp?
+                  ($.db/open-tmp etch-store-temp-prefix)
+                  (let [etch-store-file (java.io/file etch-store)]
+                    ;; Ensures parent directory exists.
+                    (when-let [parent (.getParentFile etch-store-file)]
+                      (when-not (.exists parent)
+                        (.mkdirs parent)))
+                    ($.db/open etch-store-file)))]
+      ($.db/global-set store)
+      (log/debug "Etch instance ready"
+                 store)
+      store)
+    ;;
     (catch Exception ex
       (throw (ex-info "Failed to create Etch store." 
-               (select-keys config [:etch-store-temp? :etch-store-temp-prefix :etch-store]) ex)))))
+                      (select-keys config
+                                   [:etch-store
+                                    :etch-store-temp-prefix
+                                    :etch-store-temp?])
+                      ex)))))
