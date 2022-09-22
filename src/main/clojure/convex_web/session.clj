327539bffc62e8b36ee7e38ffe89d375c393287e
Adam Helins <adam@protosens.com>
2022-09-22 19:43:04 +0200

    Refactor `convex-web.session`

--- src/main/clojure/convex_web/session.clj
+++ src/main/clojure/convex_web/session.clj
@@ -1,81 +1,119 @@
 (ns convex-web.session
-  (:require
-   [clojure.pprint :as pp]
-   [clojure.tools.logging :as log]
 
-   [datalevin.core :as d]
-   [ring.middleware.session.store :refer [SessionStore]]
+  "User session management."
 
-   [convex-web.convex :as convex])
+  (:require [clojure.pprint                :as pprint]
+            [clojure.tools.logging         :as log]
+            [convex-web.convex             :as $.web.convex]
+            [datalevin.core                :as d]
+            [ring.middleware.session.store :as ring.middleware.session.store]))
 
-  (:import (java.util UUID)))
 
-(defn all [db]
-  (d/q '[:find [(pull ?e [*]) ...]
-         :in $
+;;;;;;;;;;
+
+
+(defn all
+  
+  [db]
+
+  (d/q '[:find  [(pull ?e [*]) ...]
+         :in    $
          :where [?e :convex-web.session/id _]]
        db))
 
-(defn find-session [db id]
-  (when-let [session (d/q '[:find (pull ?e [*]) .
-                            :in $ ?id
-                            :where [?e :convex-web.session/id ?id]]
-                       db id)]
-    (let [{wallet :convex-web.session/wallet} session
 
-          ;; TODO: Change the app to use :convex-web.session/wallet instead.
-          session (assoc session :convex-web.session/accounts (vec wallet))]
-      session)))
 
-(defn find-account [db {:keys [sid address]}]
-  (let [wallet (d/q '[:find ?wallet .
-                      :in $ ?sid
-                      :where
-                      [?session :convex-web.session/id ?sid]
-                      [?session :convex-web.session/wallet ?wallet]]
-                 db sid)]
-    (reduce
-      (fn [_ {this-address :convex-web.account/address :as account}]
-        (when (= (convex/address-safe address) (convex/address-safe this-address))
-          (reduced account)))
-      nil
-      wallet)))
+(defn all-ring
 
-(defn all-ring [db]
-  (d/q '[:find [(pull ?e [*]) ...]
-         :in $
+  [db]
+
+  (d/q '[:find  [(pull ?e [*]) ...]
+         :in    $
          :where [?e :ring.session/key _]]
        db))
 
-(defn find-ring-session [db key]
-  (d/q '[:find (pull ?e [*]) .
-         :in $ ?key
+
+
+(defn find-account
+
+  [db {:keys [address
+              sid]}]
+
+  (let [wallet (d/q '[:find  ?wallet .
+                      :in    $ ?sid
+                      :where
+                      [?session :convex-web.session/id ?sid]
+                      [?session :convex-web.session/wallet ?wallet]]
+                 db
+                 sid)]
+    (reduce (fn [_acc account]
+              (when (= ($.web.convex/address-safe address)
+                       ($.web.convex/address-safe (account :convex-web.account/address)))
+                (reduced account)))
+            nil
+            wallet)))
+
+
+
+(defn find-ring-session
+
+  [db key]
+
+  (d/q '[:find  (pull ?e [*]) .
+         :in    $ ?key
          :where [?e :ring.session/key ?key]]
-       db key))
+       db
+       key))
 
 
-(deftype PersistentSessionStore [conn]
-  SessionStore
-  (read-session [_ key]
-    (find-ring-session @conn key))
 
-  (write-session [_ key data]
-    (let [;; Key is nil when it's a new Session
-          key (or key (str (UUID/randomUUID)))
-          session (merge {:ring.session/key key} data)]
+(defn find-session [db id]
+  (when-let [session (d/q '[:find  (pull ?e [*]) .
+                            :in    $ ?id
+                            :where [?e :convex-web.session/id ?id]]
+                          db
+                          id)]
+    (assoc session
+           ;; TODO. Change the app to use `:convex-web.session/wallet` as well.
+           :convex-web.session/accounts
+           (session :convex-web.session/wallet))))
 
-      (log/debug (str "Write Ring session:\n" (with-out-str (pp/pprint session))))
 
-      (d/transact! conn [session])
+;;;;;;;;;;
 
-      key))
 
-  (delete-session [_ key]
-    (log/debug "Delete Ring session" key)
+(deftype PersistentSessionStore
+         [conn]
 
-    (d/transact! conn [[:db.fn/retractEntity key]])
+  ring.middleware.session.store/SessionStore
 
-    nil))
+    (delete-session [_ key]
+      (log/debug "Delete Ring session"
+                 key)
+      (d/transact! conn
+                   [[:db.fn/retractEntity key]])
+      nil)
 
-(defn persistent-session-store [conn]
+    (read-session [_ key]
+      (find-ring-session @conn
+                         key))
+
+    (write-session [_ key data]
+      (let [;; Key is nil when it's a new session
+            key     (or key
+                        (str (random-uuid)))
+            session (merge {:ring.session/key key}
+                           data)]
+        (log/debug (str "Write Ring session:"
+                        \newline
+                        (with-out-str
+                          (pprint/pprint session))))
+        (d/transact! conn
+                     [session])
+        key)))
+
+
+
+(defn persistent-session-store
+  [conn]
   (PersistentSessionStore. conn))
